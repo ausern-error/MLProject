2011fb7338fa8c1b949f9590609a4f64ab83ba61
Tabish Zaheer Mukhtar <ausern.error@gmail.com>
2023-04-23 17:15:06 +0200

    inital clock implmentation

--- simulation/main.py
+++ simulation/main.py
@@ -1,10 +1,13 @@
 import time
 import entity_structures
+import clock
 last_tick_time = time.time()
 tick_speed = 60
+clock = clock.Clock(180)
 while True:
     sleep = 1 / tick_speed - (time.time() - last_tick_time)
     last_tick_time = time.time()
     if sleep > 0:
         time.sleep(sleep)
- 
+        clock.tick()
+        print(clock.day_counter)
