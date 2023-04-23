1f349999c86d4a8bd2ca76bea7b5ad42792356e9
Tabish Zaheer Mukhtar <ausern.error@gmail.com>
2023-04-23 16:47:39 +0200

    expand entity class

--- entity_structures.py
+++ entity_structures.py
@@ -3,9 +3,19 @@ from dataclasses import dataclass
 
 class Vector2:
 #placeholder, will be replaced when we decide on math library
-	x: int
-	y: int
+    x: int
+    y: int
 
 @dataclass
-class Entity:
-	position: Vector2
+class entity:
+    position: Vector2
+    animal_type: str
+    age: int
+    max_age: int
+    food: int
+    food_consumption: int
+    reproduction_rate: int
+    days_since_reproduction: int
+    children: list #TODO: Type annotate these
+    parents: list
+
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import time
+import entity_structures
 last_tick_time = time.time()
 tick_speed = 60
 while True:
-	sleep = 1 / tick_speed - (time.time() - last_tick_time)
-	last_tick_time = time.time()
-	if sleep > 0:
-		print("tick!")
-		time.sleep(sleep)
+    sleep = 1 / tick_speed - (time.time() - last_tick_time)
+    last_tick_time = time.time()
+    if sleep > 0:
+        time.sleep(sleep)
+ 
