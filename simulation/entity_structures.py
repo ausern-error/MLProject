8df7fb8ba00042fc79372be5705898d6fe996498
ausern <ausern.error@gmail.com>
2023-05-14 14:57:06 +0200

    Add task system

--- simulation/entity_structures.py
+++ simulation/entity_structures.py
@@ -1,9 +1,17 @@
 from dataclasses import dataclass
+from enum import Enum
 @dataclass
 class Vector2:
 #placeholder, will be replaced when we decide on math library
     x: int
     y: int
+
+class Task(Enum):
+    wander = 1
+    gather = 2
+    reproduce = 3
+    hunt = 4
+    escape = 5
 
 @dataclass
 class Entity:
@@ -17,3 +25,15 @@ class Entity:
     days_since_reproduction: int
     children: list #TODO: Type annotate these
     parents: list
+    task: Task
+    def wander():
+        pass
+    def gather():
+        pass
+    def reproduce():
+        pass
+    def hunt():
+        pass
+    def escape():
+        pass
+    
